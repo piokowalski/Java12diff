a8fa1d216ee7d6487adab318bb8d5ec96c734e04
piokowalski <piokowalski@onet.pl>
2019-04-04 14:28:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var list = List.of(1, 2, 3, 4);
+        System.out.println(list);
+
+    }
+}
